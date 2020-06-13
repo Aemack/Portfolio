601f8b1c3e06fe7d4109622911d50a219db57d82
Aemack <64653970+Aemack@users.noreply.github.com>
2020-06-13 13:39:18 +0100

    Adding most used word button

--- LyricAnalyzer/js/index.js
+++ LyricAnalyzer/js/index.js
@@ -1,33 +1,41 @@
+function hide_buttons(){
+    button1 = document.getElementById("btn1");
+    button2 = document.getElementById("btn2");
+    button3 = document.getElementById("btn3");
+    button1.style.visibility = "hidden";
+    button2.style.visibility = "hidden";
+    button3.style.visibility = "hidden";
+
+}
+
+
+function cant_find_song(){
+    lyricBlock = document.getElementById("lyrics")
+    lyricBlock.innerHTML = "Could not locate lyrics!"
+    
+    hide_buttons()
+}
+
+
 function reqListener() {
-    console.log(this.responseText)
     if (this.responseText.startsWith("<")){
             
-        lyricBlock = document.getElementById("lyrics")
-        lyricBlock.innerHTML = "Could not locate lyrics!"
-        
-        button1 = document.getElementById("btn1");
-        button2 = document.getElementById("btn2");
-        button1.style.visibility = "hidden";
-        button2.style.visibility = "hidden"
+        cant_find_song()
 
     } else {
         var data = JSON.parse(this.responseText);
             if(data.result == undefined){
                 console.log("Hello world")
-                lyricBlock = document.getElementById("lyrics")
-                lyricBlock.innerHTML = "Could not locate lyrics!"
-                
-                button1 = document.getElementById("btn1");
-                button2 = document.getElementById("btn2");
-                button1.style.visibility = "hidden";
-                button2.style.visibility = "hidden"
+                cant_find_song();
             } else {
             lyricBlock = document.getElementById("lyrics")
             lyricBlock.innerHTML = data.result.track.text
-            button1 = document.getElementById("btn1");
-            button2 = document.getElementById("btn2");
-            button1.style.visibility = "visible";
-            button2.style.visibility = "visible"
+        button1 = document.getElementById("btn1");
+        button2 = document.getElementById("btn2");
+        button3 = document.getElementById("btn3");
+        button1.style.visibility = "visible";
+        button2.style.visibility = "visible";
+        button3.style.visibility = "visible";
         }
     }
 
@@ -48,16 +56,19 @@ function get_lyrics(artist, track){
 function get_words(){
     var lyrics = document.getElementById("lyrics").innerHTML;
     if (lyrics){
-    return split_string(lyrics);
+        return split_string(lyrics);
     } else {
         return "Cannot find lyrics"
     }
 }
 
 function getOccurence(array, value) {
-    var count = 0;
-    array.forEach((v) => (v=== value && count++));
-    return count;
+    if (array) {
+        //console.log(array)
+        var count = 0;
+        array.forEach((v) => (v=== value && count++));
+        return count;
+    }
 }
 
 function split_string(str) {
@@ -65,7 +76,7 @@ function split_string(str) {
     str= str.split(re).join('')
     re = /\(.*?\)/;
     str= str.split(re).join('')
-    console.log(str)
+    //console.log(str)
     str  = str.split('\n').join(' ')
     str = str.split(/[?,.\-_:()]+/).join('')
     words = str.split(" ")
@@ -89,6 +100,42 @@ function clear_page(){
     outputList.forEach((elem) => {
         elem.remove();
     })
+}
+
+function unique_word_count() {
+    clear_page();
+    words = get_words();
+    origWords = find_original_words(words);
+    print_unique_word_count(origWords.length)
+}
+
+function unique_word_list() {
+    clear_page();
+    words = get_words();
+    origWords = find_original_words(words);
+    print_unique_words(origWords);
+}
+
+function most_used_word(){
+    mostUsed = ""; 
+    wordList = get_words();
+    wordList.forEach(function(word){
+        word_occurence = getOccurence(wordList, word)
+        if (word_occurence > getOccurence(wordList, mostUsed)){
+            mostUsed = word;
+        }
+
+    })
+    return mostUsed;
+}
+
+function display_lyrics(){
+    clear_page()
+    artist = document.getElementById("artist").value;
+    track = document.getElementById("track").value;
+    get_lyrics(artist,track)
+
+
 }
 
 function print_unique_words(wordList){
@@ -123,25 +170,13 @@ function print_unique_word_count(wordCount){
     outputList.appendChild(wordCountElement)
 }
 
-function unique_word_count() {
+function print_most_used_word(){
     clear_page();
-    words = get_words();
-    origWords = find_original_words(words);
-    print_unique_word_count(origWords.length)
-}
-
-function unique_word_list() {
-    clear_page();
-    words = get_words();
-    origWords = find_original_words(words);
-    print_unique_words(origWords);
-}
-
-function display_lyrics(){
-    clear_page()
-    artist = document.getElementById("artist").value;
-    track = document.getElementById("track").value;
-    get_lyrics(artist,track)
-
-
+    wordList = get_words();
+    mostUsedWord = most_used_word();
+    wordOccurence = getOccurence(wordList, mostUsedWord);
+    mostUsedElement = document.createElement("li");
+    mostUsedElement.setAttribute("class", "subList");
+    mostUsedElement.innerHTML = "Most used word: "+ mostUsedWord + " - "+ wordOccurence;
+    outputList.appendChild(mostUsedElement)
 }
