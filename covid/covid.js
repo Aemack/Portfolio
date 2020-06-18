67e7ba8016791302218a7c1e0acd0d1479cc6a1d
Aemack <64653970+Aemack@users.noreply.github.com>
2020-06-18 12:47:25 +0100

    Adding starting code for covid tracker

---
+++ covid/covid.js
@@ -0,0 +1,145 @@
+
+
+function clear_page(){
+    destroy_chart()
+
+    outputList = document.getElementById("output").querySelectorAll(".output")
+    outputList.forEach((elem) => {
+        elem.remove();
+    })
+}
+
+function destroy_chart(){
+    container = document.getElementById("container")
+    chart =document.getElementById("myChart")
+    chart.remove();
+    newChart = document.createElement("canvas")
+    newChart.setAttribute("id","myChart")
+    container.appendChild(newChart)
+}
+function display_country_data(data){
+    
+    todaysData = data[data.length-1]
+    output = document.getElementById("output")
+    nameElement = document.createElement("h5")
+    nameElement.setAttribute("class","output")
+    nameElement.innerHTML = `${todaysData.Country}:`
+    nameElement.setAttribute("id","nameElement")
+    output.appendChild(nameElement)
+
+    deathElement = document.createElement("h3")
+    deathElement.setAttribute("class","output")
+    deathElement.innerHTML = `Deaths: ${todaysData.Deaths}.`
+
+    activeElement = document.createElement("h4")
+    activeElement.setAttribute("class","output")
+    activeElement.innerHTML = `Active: ${todaysData.Active}.`
+
+    confirmedElement = document.createElement("h5")
+    confirmedElement.setAttribute("class","output")
+    confirmedElement.innerHTML = `Confirmed: ${todaysData.Confirmed}.`
+    
+    nameElement =document.getElementById("nameElement")
+    nameElement.appendChild(deathElement)
+    nameElement.appendChild(activeElement)
+    nameElement.appendChild(confirmedElement)
+
+    active = []
+    date = []
+    deaths = []
+
+    data.forEach(dayData => {
+        active.push(dayData.Active)
+        date.push(dayData.Date.split("T")[0])
+        deaths.push(dayData.Deaths)
+    })
+    
+
+    let pieChart = new Chart(myChart, {
+        type:'line',
+        data:{
+            labels:date,
+            datasets:[{
+                label: "Active Cases",
+                data:active,
+                backgroundColor:'yellow'
+            },{
+                label: "Deaths",
+                data:deaths,
+                backgroundColor:'red'
+            }],
+        },
+        options:{},
+    });
+    
+}
+
+function country_clicked() {
+    clear_page()
+    country = document.getElementById("country").value;
+    fetch(`https://api.covid19api.com/total/country/${country}`)
+    .then(function (resp) { return resp.json()})
+    .then(function (data){
+        display_country_data(data)
+        }
+    )
+}
+
+function world_graph_clicked(){
+    clear_page();
+    
+    fetch(`https://api.covid19api.com/summary`)
+    .then(function (resp) {return resp.json()} )
+    .then(function (data) { 
+        world_graph(data)
+     })
+}
+
+function world_graph (data) {
+    let myChart = document.getElementById("myChart").getContext("2d")
+    pieChart = new Chart(myChart)
+    
+    countryLabels = []
+    countryDeaths =[]
+    countryColors = []
+    data.Countries.forEach(country =>{
+        countryLabels.push(country.Country)
+        countryDeaths.push(country.TotalDeaths)
+        rand1 = Math.floor(Math.random()*255)
+        rand2 = Math.floor(Math.random()*255)
+        rand3 = Math.floor(Math.random()*255)
+        countryColors.push(`rgba(${rand1},${rand2},${rand3})`)
+
+    })
+    
+    pieChart = new Chart(myChart, {
+        type:'bar',//'horizontalBar',
+        data:{
+            labels:countryLabels,
+            datasets:[{
+                label: "Deaths",
+                data:countryDeaths,
+                backgroundColor:countryColors,
+                hoverBackgroundColor:'green'
+            }],
+        },
+        options:{},
+    });
+
+
+}
+
+/*
+//Get summary and create element with total global deaths
+window.onload = function(){
+    fetch(`https://api.covid19api.com/summary`)
+    .then(function (resp) {return resp.json()} )
+    .then(function (data) { 
+        var totalGlobalDeaths = document.createElement("h3")
+        totalGlobalDeaths.setAttribute("class","output")
+        totalGlobalDeaths.setAttribute("id","totalGlobalDeaths")
+        totalGlobalDeaths.innerHTML = "Total Global Deaths: "+data.Global.TotalDeaths
+        ouput = document.getElementById("ouput")
+        document.output.appendChild(totalGlobalDeaths)
+    })
+}*/
