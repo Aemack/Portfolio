3f99b01ad9401609fe1a4f6aaaf33ccec226a5b5
Aemack <64653970+Aemack@users.noreply.github.com>
2020-06-22 13:35:11 +0100

    editing picture animation, adding data

--- kathysk9/scripts/k9.js
+++ kathysk9/scripts/k9.js
@@ -37,7 +37,7 @@ function display_fix_options(cause){
 	mainBody = document.getElementById("causeDescription")
 	switch (cause) {
 		case "Frustration":
-			fixOptions=["Check the Enviroment","Check Routines","Consider Exercise","Consider Boredom","Remove Cause","Consider Breed of Dog","Consider Dog training"];
+			fixOptions=["Check the Enviroment","Check Daily Structure","Consider Exercise","Consider Boredom","Remove Cause","Consider Breed of Dog","Consider Dog training"];
 			break;
 		case "Fear":
 			fixOptions=["Visit a Behaviourist","Establish Trigger","Multi Stacking Triggers","Counter-Condition","Desensitise to Trigger","Teach Calming Protocol"]
@@ -52,7 +52,7 @@ function display_fix_options(cause){
 			fixOptions=["Vet Approved Hormone Treatment","Spay","Consider Calming Diffuser","Consider a Safe Haven"]
 			break;
 		case "Puppy Biting":
-			fixOptions=["Puppy Training Classes","Remove Interaction","Distract and Reward","Find a Mutually Exclusive Behaviour","Consider Enough Rest","Consider Calming Protocols","Consider a Safe Haven","Spay"]
+			fixOptions=["Puppy Training Classes","Remove Interaction","Distract and Reward","Mutually Exclusive Behaviour","Consider Enough Rest","Consider Calming Protocols","Consider a Safe Haven","Spay"]
 			break;
 		case "Redirected Aggression":
 			fixOptions = ["Visit a Behaviourist"]
@@ -152,18 +152,23 @@ function fix_description(fix) {
 			break;
 		case "Visit a Behaviourist":
 			return "A canine behaviourist is someone who has trained to help dog guardians understand why their dog is behaving in the way it is. They will discuss the issues and do a lot of detective work to find out why your dog behaves the way it does. They will then suggest a programme for you and your dog and support you through it if necessary"
-		case "Check Routines":
+		case "Check Daily Structure":
 			return "Dog's live their lives with very little choice, they rely on us to know their needs and meet them. It is important that dogs have daily schedules rather than routines. Schedules mean that mostly you will get walked twice a day, but sometimes 9-30 sometimes 10, it will happen but not at a precise time.  It is important that the dog knows his needs will be met, but he should not be pinned down to a routine so that when he cannot be fed at 5 O’clock on the dot, he will not get frustrated or anxious. However, routines are important for puppies and new dogs as they acclimatise themselves to living with you. Routines for toilet and feed times etc will be useful, but you will gradually want to loosen them into structures rather than routines"
 			break;
 		case "Consider Exercise":
 			return "It is important to understand your dog breed and allow them the amount of exercise that they require. Each breed is different, but do not assume that a small breed will need less than a large breed. Without an outlet for their energy and an opportunity to engage in their natural behaviours, they may become frustrated and bored";
 			break;
 		case "Remove Cause":
-			return "See if you can remove whatever is causing the dog to be upset or it may maul a child"
+			return "If you have managed to identify what is causing your dog’s frustration, do what you can to remove the cause. Seek professional help, if necessary, from a trained behaviourist. Consider if you are meeting your dog’s breed specific needs. Each breed of dog has been hard wired for thousands of years to complete certain tasks. The collie has eye stalk, rounding and chasing, herding motions, the spaniel to flush out birds, the greyhound to chase and kill small creatures, the terriers to hunt out vermin etc When we take these dogs into our home and treat them as lap dogs, they become frustrated as they cannot express their hardwired desires. It is necessary for guardians to find safe outlets for these behaviours. Ball sheep games for the collie, a fast run for the greyhounds, woodland adventures for the spaniel, a place to dig for a terrier etc"
 			break;
 		case "Manage the Situation":
 			return "Manage the situation and be aware of your surroundings. If you are in a play park, the dog may maul a child"
 			break;
+		case "Consider Boredom":
+			return "Most dogs are bred to do a job and many are hardwired to do this job. When there is no opportunity for the dog to use its brain boredom can set in. When this happens, the dog begins to create its own activities and these can often be the behaviours we least want. Make sure that your dog has enough company, dogs do not do well on their own, they are sociable animals. Consider enriching their environment with chews, interactive toys. Teach them tricks and brain games. Think about feeding their dinner from a snuffles rug or scatter feed. All these things can give your dog something to do when you cannot give him attention. However, play and interaction with you is always going to be the best boredom buster, so set aside time for your dog. Consider agility, scent work, training classes etc. to give you an opportunity to form a team together"
+			break;
+		case "Consider a Sport":
+			return "Allowing your dog to have an outlet for their natural instincts can be hard, but if you join a club this can sometimes be easier. Train a gun dog to the gun (reward based training only, working dogs are no different to pet dogs and should be trained along the same principles, actual hunting and killing is not necessary) Train agility, most dogs love this activity, just make sure you get correct training so you do not damage your dog’s limbs. Scent work, all dogs love using their nose and most can be taught to do this extremely well. There are great competitions that can be done. Man-trailing is also a sport that your dog might enjoy. Have a look for anything that you think you and your dog might enjoy, just remember it is about both you and your dog"
 		case "Spay":
 			return "Make it have babies no more"
 			break;
@@ -184,7 +189,7 @@ function fix_image(fix){
 		case "Check the enviroment":
 			return "imgs/bitingDog.jpg"
 			break;
-		case "Check Routines":
+		case "Check Daily Structure":
 			return "imgs/dog-routine.jpg"
 			break;
 		default:
